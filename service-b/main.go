41417ecc32266c0d350a178bc6275f33a18104b4
Peter Hrvola <peter.hrvola@oracle.com>
2022-06-22 14:37:35 +0200

    init

---
+++ service-b/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+type CounterHandler struct {
+    counter int
+}
+
+func (ct *CounterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(ct.counter)
+    ct.counter++
+    fmt.Fprintln(w, "Counter:", ct.counter)
+}
+
+func main() {
+	port := "8080"
+	if p := os.Getenv("MONGO_PASS"); p != "" {
+		port = p
+	}
+    th := &CounterHandler{counter: 0}
+    http.Handle("/", th)
+    http.ListenAndServe(":" + port, nil)
+}
