6406361b4fc0f202c1b327b956a69d1cdc7be898
Peter Hrvola <peter.hrvola@oracle.com>
2022-06-23 14:24:03 +0200

    fix

--- service-b/main.go
+++ service-b/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 type CounterHandler struct {
-    counter int
+	counter int
 }
 
 func (ct *CounterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(ct.counter)
-    ct.counter++
-    fmt.Fprintln(w, "Counter:", ct.counter)
+	fmt.Println(ct.counter)
+	ct.counter++
+	fmt.Fprintln(w, "Service B - Counter:", ct.counter)
 }
 
 func main() {
@@ -21,7 +21,7 @@ func main() {
 	if p := os.Getenv("MONGO_PASS"); p != "" {
 		port = p
 	}
-    th := &CounterHandler{counter: 0}
-    http.Handle("/", th)
-    http.ListenAndServe(":" + port, nil)
+	th := &CounterHandler{counter: 0}
+	http.Handle("/", th)
+	http.ListenAndServe(":"+port, nil)
 }
